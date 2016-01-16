9db1d35ea9b0b023f209575796163613d648f862
贾顺名 <jiashunming@outlook.com>
2016-01-16 16:33:21 +0800

    express获取上传文件的信息

---
+++ 2016-1-16/upload.js
@@ -0,0 +1,44 @@
+"use strict";
+var express = require("express"),
+	bodyParser = require("body-parser"),
+	multipart = require('connect-multiparty'),
+	multipartMiddleware = multipart(),
+	app = express();
+app.use(bodyParser({ keepExtensions: true, uploadDir: './' }));
+app.get("/", (req, res) => {
+	res.send(`
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>upload</title>
+			<meta charset="utf-8">
+		</head>
+		<body>
+			<form action="/" method="post" enctype="multipart/form-data">
+				<input type="file" name="myFile" multiple/>	
+				<button>upload this file</button>
+			</form>
+		</body>
+
+		</html>
+	`);
+});
+app.post("/", multipartMiddleware, (req, res) => {
+	res.send(`
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>upload</title>
+			<meta charset="utf-8">
+		</head>
+		<body>
+			<h1>file name: ${req.files.myFile.name}</h1>
+			<h2>file type: ${req.files.myFile.type}</h2>
+		</body>
+
+		</html>
+	`);
+})
+app.listen(12306, () => {
+	console.log("run as 12306");
+});
